6824b7eeace390f0e0536a479a16362bf67a0e72
Pere Grinus <gregjleighton@gmail.com>
2020-12-07 11:50:55 +0000

    applied buttons to HTML and present date, with time, via JavaScript. Must now execute positioning and functionality of save buttons, and work on local storage.

--- script.js
+++ script.js
@@ -1,19 +1,11 @@
-
 // jQuery ready method, ensures functions are available after the document is loaded.
-
 $(document).ready(function() {
 
-    //1.) WHEN I open the planner THEN the current day is displayed at the top of the calendar
+    // Displays current date and time under description of Tabula Rasa Application
+const todaysDate = dayjs();
 
-    // array hours available in the application
-    // const dayHours = ['06am', '07am', '08am', '09am', '10am', '11am', '12pm', '01pm', '02pm', '03pm', '04pm', '05pm', '06pm'];
-
-    // const presentDateAndTime = () => {
-    //     let presentTime = dayjs().format('dddd MMMM YYYY hh mm ss a');
-    //     $("#currentDay").html(now);
-    //     setTimeout(function() {presentDateAndTime(); }, 1000);
-    // }
-
+$("#currentDay").text(todaysDate.format(" dddd, MMMM DD, YYYY. hh:mm a"))
+});
 // 2.) WHEN I scroll down THEN I am presented with time blocks for standard business hours
 
 // 3.) WHEN I view the time blocks for that day THEN each time block is color-coded to indicate whether it is in the past, present, or future
@@ -24,4 +16,4 @@ $(document).ready(function() {
 
 // 6.) WHEN I refresh the page THEN the saved events persist
 
-});
+// });
