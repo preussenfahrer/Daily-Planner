705cfe064808febbb9fc8fa1b3a6474f0d20a28a
Pere Grinus <gregjleighton@gmail.com>
2020-12-07 09:55:49 -0500

    made text boxes work and appropriately centered them, in addition to aligning buttons on the right side of the app. Used jQuery to add some more functionality.

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 // jQuery ready method, ensures functions are available after the document is loaded.
-$(document).ready(function() {
-
+$(document).ready(function () {
     // Displays current date and time under description of Tabula Rasa Application
-const todaysDate = dayjs();
+    const todaysDate = dayjs();
+    $("#currentDay").text(todaysDate.format("dddd, MMMM D, YYYY."))
 
-$("#currentDay").text(todaysDate.format(" dddd, MMMM DD, YYYY. hh:mm a"))
+    // WHEN I view the time blocks for that day THEN each time block is color-coded to indicate whether it is in the past, present, or future
+
+    $(".saveBtn").addClass("ml-auto");
 });
-// 2.) WHEN I scroll down THEN I am presented with time blocks for standard business hours
 
-// 3.) WHEN I view the time blocks for that day THEN each time block is color-coded to indicate whether it is in the past, present, or future
+
+
+
 
 // 4.) WHEN I click into a time block THEN I can enter an event
 
