736ca1698847bdea780d6fc2435b96cb0d1deb20
Pere Grinus <gregjleighton@gmail.com>
2020-12-08 01:14:51 +0000

    attemtpted to get local storage working

--- script.js
+++ script.js
@@ -5,17 +5,11 @@ $(document).ready(function () {
     $("#currentDay").text(todaysDate.format("dddd, MMMM D, YYYY."))
 
     // WHEN I view the time blocks for that day THEN each time block is color-coded to indicate whether it is in the past, present, or future
-
     $(".saveBtn").addClass("ml-auto");
+
+
 });
 
-
-
-
-
-// 4.) WHEN I click into a time block THEN I can enter an event
-
-// 5.) WHEN I click the save button for that time block THEN the text for that event is saved in local storage
 
 // 6.) WHEN I refresh the page THEN the saved events persist
 
